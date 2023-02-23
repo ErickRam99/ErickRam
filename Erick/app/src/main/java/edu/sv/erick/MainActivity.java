ec5c69e628fdc976cf5fc1177fc17d4338b1d256
ErickRam99 <2521162019@mail.utec.edu.sv>
2023-02-22 20:19:16 -0600

    boton

--- Erick/app/src/main/java/edu/sv/erick/MainActivity.java
+++ Erick/app/src/main/java/edu/sv/erick/MainActivity.java
@@ -3,12 +3,42 @@ package edu.sv.erick;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtUser,edtContra;
+    TextView txvRes;
+    Button btnIngresar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtUser=findViewById(R.id.edtuser);
+        edtContra=findViewById(R.id.edtContrase);
+        btnIngresar=findViewById(R.id.btnLogin);
+        txvRes=findViewById(R.id.txvMensa);
+
+
+    }
+    public void ingresar(View view){
+        String user,contra,respuesta;
+
+        user=edtUser.getText().toString();
+        contra= edtContra.getText().toString();
+
+        if(user.equals("parcialETps1") && (contra.equals("p4rC14l#tp$"))) {
+            txvRes.setText("usuario y contrasena correcto");
+
+        }
+        else
+            txvRes.setText("usuario y contrasena incorrecto");
+
+
     }
 }
